8b12622e0f90c4896f7a345b117053b874d97fa5
xsjk <xsjk17@outlook.com>
2025-08-15 12:33:21 +0800

    Refactor core

--- core.py
+++ core.py
@@ -1,41 +1,48 @@
 from dataclasses import dataclass
+from enum import IntEnum, auto
 from typing import Literal
 
 import numpy as np
 import torch
-from tqdm import tqdm
+from tqdm import trange
 
 
 @dataclass
-class SBHistoryArray:
+class StateArray:
     x: np.ndarray
     y: np.ndarray
     g: np.ndarray
     V: np.ndarray
-    X: np.ndarray | None = None
-    H: np.ndarray | None = None
-    cut: np.ndarray | None = None
+    X: np.ndarray
+    H: np.ndarray
+    cut: np.ndarray
+    steps: int
 
     @property
     def best_x(self):
+        if self.cut.ndim == 0:
+            return np.sign(self.x)
         assert self.cut is not None, "cut is None"
         return np.sign(self.x[self.cut.argmax()])
 
 
 @dataclass
-class SBHistoryTensor:
+class StateTensor:
     x: torch.Tensor
     y: torch.Tensor
     g: torch.Tensor
     V: torch.Tensor
-    X: torch.Tensor | None = None
-    H: torch.Tensor | None = None
-    cut: torch.Tensor | None = None
+    X: torch.Tensor
+    H: torch.Tensor
+    cut: torch.Tensor
+    steps: int
 
     @property
     def best_x(self):
+        if self.cut.ndim == 0:
+            return self.x.sign()
         assert self.cut is not None, "cut is None"
-        return torch.sign(self.x[self.cut.argmax()])
+        return self.x[self.cut.argmax()].sign()
 
     def to(self, device):
         for attr_name, attr_value in self.__dict__.items():
@@ -44,17 +51,17 @@ class SBHistoryTensor:
         return self
 
     def numpy(self):
-        return SBHistoryArray(**{attr_name: (attr.cpu().numpy() if isinstance(attr := getattr(self, attr_name), torch.Tensor) else attr) for attr_name in self.__dict__})
+        return StateArray(**{attr_name: (attr.cpu().numpy() if isinstance(attr := getattr(self, attr_name), torch.Tensor) else attr) for attr_name in self.__dict__})  # type: ignore
 
     @property
     def device(self):
         return list({a.device for attr_name in self.__dict__ if isinstance(a := getattr(self, attr_name), torch.Tensor)})
 
     def __getitem__(self, item):
-        return SBHistoryTensor(**{attr_name: getattr(self, attr_name)[item] for attr_name in self.__dict__ if isinstance(getattr(self, attr_name), torch.Tensor)})
+        return StateTensor(**{attr_name: getattr(self, attr_name)[item] for attr_name in self.__dict__ if isinstance(getattr(self, attr_name), torch.Tensor)})
 
 
-torch.serialization.add_safe_globals([SBHistoryTensor])
+torch.serialization.add_safe_globals([StateTensor])
 
 
 def qform(J, x1, x2=None):
@@ -86,168 +93,165 @@ def qform(J, x1, x2=None):
     # The latter ('bi,ij,bj->b'), as used here, avoids this issue and is more memory efficient.
 
 
-def _aSB(J: torch.Tensor, x0: torch.Tensor, y0: torch.Tensor, beta: float, xi: float, eta: float, max_steps: int, progress_bar: bool) -> SBHistoryTensor:
+class Field(IntEnum):
+    A = auto()  # Use quadratic term in the potential
+    B = auto()  # No quadratic term in the potential but add a boundary at |x_i|=1 for all i
+
+
+class InitMode(IntEnum):
+    CENTER = auto()  # Initialize x close to the origin
+    BOUNDARY = auto()  # Initialize x at the boundaries (|x_i|=1 for all i)
+    ONES = auto()  # Initialize x as 1 (x_i=1 for all i)
+
+
+class CoupleMode(IntEnum):
+    DEFAULT = auto()  # Use J @ x as the coupling term
+    SIGN = auto()  # Use J @ sign x as the coupling term
+    RANDOM = auto()  # Use J @ random x as the coupling term
+
+
+class UpdateMode(IntEnum):
+    DEFAULT = auto()
+    SIGN = auto()  # Update x with the sign of y
+    RANDOM = auto()  # Update x with random quantization
+
+
+def run_impl(
+    J: torch.Tensor,
+    field: Field,
+    init_mode: InitMode,
+    couple_mode: CoupleMode,
+    update_mode: UpdateMode,
+    beta: float,
+    eta: float,
+    xi: float | None = None,
+    max_steps: int | None = None,
+    progress_bar: bool = False,
+    seed: int = 0,
+    return_history: bool = True,
+    fix_point_x: int | None = None,
+    fix_point_y: int | None = None,
+    fix_point_g: int | None = None,
+) -> StateTensor:
     N = J.shape[0]
 
-    x_history = torch.zeros((max_steps, N), device=J.device)
-    y_history = torch.zeros((max_steps, N), device=J.device)
-    x_history[0] = x = torch.clone(x0)
-    y_history[0] = y = torch.clone(y0)
+    if xi is None:
+        xi = float(1 / 2 / J.shape[0] ** 0.5)
+    if J.ndim != 2:
+        raise ValueError("J must be a 2D array.")
+    if J.shape[0] != J.shape[1]:
+        raise ValueError("J must be a square matrix.")
 
-    iterator = tqdm(range(max_steps), desc="aSB Progress", disable=not progress_bar)
+    if max_steps is None:
+        max_steps = int(2 / beta / eta)
 
-    for i in iterator:
+    rng = torch.Generator(device=J.device)
+    rng.manual_seed(seed)
+
+    def R():
+        return torch.rand(N, device=J.device, generator=rng)
+
+    match init_mode:
+        case InitMode.CENTER:
+            x0 = (2 * torch.rand(J.shape[0], device=J.device, generator=rng) - 1) / J.shape[0]
+        case InitMode.BOUNDARY:
+            x0 = torch.randint(2, (J.shape[0],), generator=rng, dtype=torch.float32, device=J.device) * 2 - 1
+        case InitMode.ONES:
+            x0 = torch.ones(J.shape[0], dtype=torch.float32, device=J.device)
+
+    y0 = torch.zeros(J.shape[0], device=J.device)
+
+    x: torch.Tensor = torch.clone(x0)
+    y: torch.Tensor = torch.clone(y0)
+    g: torch.Tensor
+
+    if return_history:
+        x_history = torch.empty((max_steps, N), device=J.device)
+        X_history = torch.empty((max_steps, N), device=J.device)
+        y_history = torch.empty((max_steps, N), device=J.device)
+        g_history = torch.empty((max_steps, N), device=J.device)
+        x_history[0] = torch.clone(x)
+        y_history[0] = torch.clone(y)
+
+    for i in trange(max_steps, disable=not progress_bar):
         t = i * eta
 
-        g = -(x**2 + (1 - beta * t)) * x + xi * J @ x
-        x += y * eta
+        match couple_mode:
+            case CoupleMode.DEFAULT:
+                X = x
+            case CoupleMode.SIGN:
+                X = x.sign()
+            case CoupleMode.RANDOM:
+                X = (x + R()).floor()
+
+        match field:
+            case Field.A:
+                g = -(x**2 + (1 - beta * t)) * x + xi * J @ X
+            case Field.B:
+                g = -(1 - beta * t) * x + xi * J @ X
+
+        if fix_point_g:
+            scale = 2**fix_point_g
+            g = (g * scale).round() / scale
+
         y += g * eta
 
-        x_history[i] = x
-        y_history[i] = y
+        if fix_point_y:
+            scale = 2**fix_point_y
+            y = (y * scale).round() / scale
 
-        if torch.any(torch.linalg.norm(g) > 20):
+        match update_mode:
+            case UpdateMode.DEFAULT:
+                x += y * eta
+            case UpdateMode.SIGN:
+                x += y.sign() * eta
+            case UpdateMode.RANDOM:
+                x += (y + R()).floor() * eta
+
+        if fix_point_x:
+            x = (x * 2**fix_point_x).round() / 2**fix_point_x
+
+        if field == Field.B:
+            y[x.abs() > 1] = 0
+            x = x.clip(-1, 1)
+
+        if return_history:
+            x_history[i] = x
+            X_history[i] = X
+            y_history[i] = y
+            g_history[i] = g
+
+        if (x.abs() == 1).all():
             break
 
-    x_history = x_history[: i + 1]
-    y_history = y_history[: i + 1]
+    if return_history:
+        x_history = x_history[: i + 1]
+        X_history = X_history[: i + 1]
+        y_history = y_history[: i + 1]
+        g_history = g_history[: i + 1]
+        t = torch.arange(len(x_history), device=J.device) * eta
+        match field:
+            case Field.A:
+                V_history = 1 / 4 * (x_history**4).sum(-1) + (1 - beta * t) / 2 * (x_history**2).sum(-1) - xi * qform(J, x_history, X_history)
+            case Field.B:
+                V_history = (1 - beta * t) / 2 * (x_history**2).sum(-1) - xi * qform(J, x_history, X_history)
 
-    t = torch.arange(len(x_history), device=J.device)[:, None] * eta
-    g_history = -(x_history**2 + (1 - beta * t)) * x_history + xi * x_history @ J
-    V_history = 1 / 4 * (x_history**4).sum(-1) + (1 - beta * t.flatten()) / 2 * (x_history**2).sum(-1) - xi * qform(J, x_history)
+        H_history = 1 / 2 * (y_history**2).sum(-1) + V_history
+        x_history[x_history.sign() == 0] = 1
+        x_sign = x_history.sign()
+        cut_history = ((-J.sum() + qform(J, x_sign)) / 4).to(dtype=torch.int32)
+        return StateTensor(x_history, y_history, g_history, V_history, X_history, H_history, cut_history, i + 1)
 
-    return SBHistoryTensor(x_history, y_history, g_history, V_history)
-
-
-def _bSB(J: torch.Tensor, x0: torch.Tensor, y0: torch.Tensor, beta: float, xi: float, eta: float, max_steps: int, progress_bar: bool) -> SBHistoryTensor:
-    N = J.shape[0]
-
-    x_history = torch.zeros((max_steps, N), device=J.device)
-    y_history = torch.zeros((max_steps, N), device=J.device)
-    x_history[0] = x = torch.clone(x0)
-    y_history[0] = y = torch.clone(y0)
-
-    iterator = tqdm(range(max_steps), desc="bSB Progress", disable=not progress_bar)
-    for i in iterator:
-        t = i * eta
-
-        g = -(1 - beta * t) * x + xi * J @ x
-        x += y * eta
-        y += g * eta
-
-        y[torch.abs(x) > 1] = 0
-        x = torch.clip(x, -1, 1)
-
-        x_history[i] = x
-        y_history[i] = y
-
-        if (torch.abs(x) == 1).all():
-            break
-
-    x_history = x_history[: i + 1]
-    y_history = y_history[: i + 1]
-
-    t = torch.arange(len(x_history), device=J.device)[:, None] * eta
-    g_history = -(1 - beta * t) * x_history + xi * x_history @ J
-    V_history = (1 - beta * t.flatten()) / 2 * (x_history**2).sum(-1) - xi * qform(J, x_history)
-
-    return SBHistoryTensor(x_history, y_history, g_history, V_history)
+    else:
+        V = (1 - beta * t) / 2 * (x**2).sum(-1) - xi * x @ J @ X
+        H = 1 / 2 * (y**2).sum(-1) + V
+        x[x.sign() == 0] = 1
+        cut = ((-J.sum() + x @ J @ x) / 4).to(dtype=torch.int32)
+        return StateTensor(x, y, g, V, X, H, cut, i + 1)
 
 
-def _dSB(J: torch.Tensor, x0: torch.Tensor, y0: torch.Tensor, beta: float, xi: float, eta: float, max_steps: int, progress_bar: bool) -> SBHistoryTensor:
-    N = J.shape[0]
-
-    x_history = torch.zeros((max_steps, N), device=J.device)
-    y_history = torch.zeros((max_steps, N), device=J.device)
-    x_history[0] = x = torch.clone(x0)
-    y_history[0] = y = torch.clone(y0)
-
-    iterator = tqdm(range(max_steps), desc="dSB Progress", disable=not progress_bar)
-    for i in iterator:
-        t = i * eta
-
-        g = -(1 - beta * t) * x + xi * J @ torch.sign(x)
-        x += y * eta
-        y += g * eta
-
-        y[torch.abs(x) > 1] = 0
-        x = torch.clip(x, -1, 1)
-
-        x_history[i] = x
-        y_history[i] = y
-
-        if (torch.abs(x) == 1).all():
-            break
-
-    x_history = x_history[: i + 1]
-    y_history = y_history[: i + 1]
-
-    t = torch.arange(len(x_history), device=J.device)[:, None] * eta
-    g_history = -(1 - beta * t) * x_history + xi * torch.sign(x_history) @ J
-    V_history = (1 - beta * t.flatten()) / 2 * (x_history**2).sum(-1) - xi * qform(J, x_history, torch.sign(x_history))
-
-    return SBHistoryTensor(x_history, y_history, g_history, V_history)
-
-
-def R(x: torch.Tensor, r: torch.Tensor) -> torch.Tensor:
-    x_floor = torch.floor(x)
-    x_ceil = torch.ceil(x)
-    x_decimal = x - x_floor
-
-    x_out = x_floor
-    x_out[r < x_decimal] = x_ceil[r < x_decimal]
-    return x_out
-
-
-def _sSB(J: torch.Tensor, x0: torch.Tensor, y0: torch.Tensor, beta: float, xi: float, eta: float, max_steps: int, progress_bar: bool, rng: torch.Generator, clip: bool = True) -> SBHistoryTensor:
-    N = J.shape[0]
-
-    x_history = torch.zeros((max_steps, N), device=J.device)
-    X_history = torch.zeros((max_steps, N), device=J.device)
-    y_history = torch.zeros((max_steps, N), device=J.device)
-    g_history = torch.zeros((max_steps, N), device=J.device)
-    x_history[0] = x = torch.clone(x0)
-    y_history[0] = y = torch.clone(y0)
-
-    iterator = tqdm(range(max_steps), desc="sSB Progress", disable=not progress_bar)
-    for i in iterator:
-        t = i * eta
-
-        X = R(x, torch.rand(N, device=J.device, generator=rng))
-        g = -(1 - beta * t) * x + xi * J @ X
-        if clip:
-            g = torch.clip(g, -1, 1)
-
-        x += R(y, torch.rand(N, device=J.device, generator=rng)) * eta
-        y += R(g, torch.rand(N, device=J.device, generator=rng)) * eta
-
-        y[torch.abs(x) > 1] = 0
-        x = torch.clip(x, -1, 1)
-
-        if clip:
-            y = torch.clip(y, -1, 1)
-
-        x_history[i] = x
-        X_history[i] = X
-        y_history[i] = y
-        g_history[i] = g
-
-        if (torch.abs(x) == 1).all():
-            break
-
-    x_history = x_history[: i + 1]
-    X_history = X_history[: i + 1]
-    y_history = y_history[: i + 1]
-    g_history = g_history[: i + 1]
-
-    t = torch.arange(len(x_history), device=J.device)[:, None] * eta
-    V_history = (1 - beta * t.flatten()) / 2 * (x_history**2).sum(-1) - xi * qform(J, x_history, X_history)
-
-    return SBHistoryTensor(x_history, y_history, g_history, V_history, X_history)
-
-
-type MethodType = Literal["aSB", "bSB", "dSB", "sSB", "sSB_clip"]
-
+MethodType = Literal["aSB", "bSB", "dSB", "sSB", "sSB_sgn"]
+InitType = Literal["center", "boundary", "ones"]
 
 def run(
     J: torch.Tensor,
@@ -257,9 +261,14 @@ def run(
     xi: float | None = None,
     eta: float = 2**-3,
     max_steps: int | None = None,
+    init: InitType = "center",
     seed: int = 0,
     progress_bar: bool = False,
-) -> SBHistoryTensor:
+    return_history: bool = True,
+    fix_point_x: int | None = None,
+    fix_point_y: int | None = None,
+    fix_point_g: int | None = None,
+) -> StateTensor:
     """
     Run the SB algorithm.
 
@@ -267,8 +276,8 @@ def run(
     ----------
     J : torch.Tensor
         The input ising matrix (neg of the coupling matrix).
-    method : str
-        The method to use. One of "aSB", "bSB", "dSB", "sSB", "sSB_clip".
+    method : MethodType
+        The method to use. One of MethodType.
     beta : float
         The increasing rate of the p(t)
     xi : float
@@ -277,78 +286,69 @@ def run(
         The time step.
     max_steps : int
         The maximum number of steps to run.
+    init : InitType
+        The initial state of x. One of InitType.
     seed : int
         The random seed.
     progress_bar : bool
         Whether to show a progress bar.
+    return_history : bool
+        Whether to record the history of the optimization process.
+    fix_point_x : int | None
+        The number of decimal places to round x to.
+    fix_point_y : int | None
+        The number of decimal places to round y to.
+    fix_point_g : int | None
+        The number of decimal places to round g to.
     """
-
-    if J.ndim != 2:
-        raise ValueError("J must be a 2D array.")
-    if J.shape[0] != J.shape[1]:
-        raise ValueError("J must be a square matrix.")
-
-    if xi is None:
-        xi = 1 / 2 / J.shape[0] ** 0.5
-
-    if max_steps is None:
-        max_steps = int(2 / beta / eta)
-
-    # Type assertions after None checks
-    assert xi is not None
-    assert max_steps is not None
-
-    N = J.shape[0]
-
-    rng = torch.Generator(device=J.device)
-    rng.manual_seed(seed)
-
-    x0 = (2 * torch.rand(N, device=J.device, generator=rng) - 1) / N
-    y0 = torch.zeros(N, device=J.device)
 
     match method:
         case "aSB":
-            r = _aSB(J, x0, y0, beta, xi, eta, max_steps, progress_bar)
+            # Adaptive Simulated Bifurcation
+            field = Field.A
+            couple_mode = CoupleMode.DEFAULT
+            update_mode = UpdateMode.DEFAULT
         case "bSB":
-            r = _bSB(J, x0, y0, beta, xi, eta, max_steps, progress_bar)
+            # Ballistic Simulated Bifurcation
+            field = Field.B
+            couple_mode = CoupleMode.DEFAULT
+            update_mode = UpdateMode.DEFAULT
         case "dSB":
-            r = _dSB(J, x0, y0, beta, xi, eta, max_steps, progress_bar)
+            # Discrete Simulated Bifurcation
+            field = Field.B
+            couple_mode = CoupleMode.SIGN
+            update_mode = UpdateMode.DEFAULT
         case "sSB":
-            r = _sSB(J, x0, y0, beta, xi, eta, max_steps, progress_bar, rng, clip=False)
-        case "sSB_clip":
-            r = _sSB(J, x0, y0, beta, xi, eta, max_steps, progress_bar, rng, clip=True)
+            # Stochastic Simulated Bifurcation
+            field = Field.B
+            couple_mode = CoupleMode.RANDOM
+            update_mode = UpdateMode.DEFAULT
+        case "sSB_sgn":
+            # Stochastic Simulated Bifurcation that updates x with the sign of y
+            field = Field.B
+            couple_mode = CoupleMode.RANDOM
+            update_mode = UpdateMode.SIGN
         case _:
-            raise ValueError(f"Unknown method: {method}. Supported methods are: aSB, bSB, dSB, sSB.")
+            raise ValueError(f"Unknown method: {method}")
 
-    r.H = 1 / 2 * (r.y**2).sum(-1) + r.V
+    init_mode = InitMode[init.upper()]
 
-    x = torch.sign(r.x)
-    x[x == 0] = 1
-    assert ((x == 1) | (x == -1)).all(), "x must be a valid configuration of the Ising model."
-    cut = (-J.sum() + qform(J, x)) / 4
-    r.cut = cut.to(dtype=torch.int32)
-    assert (cut == r.cut).all(), "cut must be an integer value."
-    return r
-
-
-def run_numpy(
-    J: np.ndarray,
-    method: MethodType,
-    *,
-    beta: float = 2**-11,
-    xi: float | None = None,
-    eta: float = 2**-3,
-    max_steps: int | None = None,
-    seed: int = 0,
-    progress_bar: bool = False,
-) -> SBHistoryArray:
-    return run(
-        torch.tensor(J, dtype=torch.float32, device="cuda" if torch.cuda.is_available() else "cpu"),
-        method=method,
+    r = run_impl(
+        J=J,
         beta=beta,
         xi=xi,
         eta=eta,
         max_steps=max_steps,
-        seed=seed,
         progress_bar=progress_bar,
-    ).numpy()
+        seed=seed,
+        field=field,
+        init_mode=init_mode,
+        couple_mode=couple_mode,
+        update_mode=update_mode,
+        return_history=return_history,
+        fix_point_x=fix_point_x,
+        fix_point_y=fix_point_y,
+        fix_point_g=fix_point_g,
+    )
+
+    return r
